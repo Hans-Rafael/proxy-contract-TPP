fc68802c1d91e3ac2ac9da9f0905e5f165e24995
Hans <hansvekoni@gmail.com>
2025-01-06 19:48:09 -0300

    deployed ready for version 1 contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,34 @@
 //author: Hans Garcia
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
-     // Obtennemos el contrato a desplegar
-     const BasicContractV1 = await ethers.getContractFactory("BasicContractV1");
-     console.log("Desplegando el contrato BasicContractV1...");
-     // Desplegamos el contrato
-     const basicContractV1 = await BasicContractV1.deploy();
-     // Esperar a que el contrato sea minado
-     await basicContractV1.waitForDeployment();
-     // Obtenemos la dirección del contrato desplegado
-    const contractAddress = await basicContractV1.getAddress();
-    console.log("BasicContractV1 deployed to:", contractAddress);
+  //// Deploy LogicV1
+  // Obtennemos el contrato a desplegar
+  const BasicContractV1 = await ethers.getContractFactory("BasicContractV1");
+  console.log("Desplegando el contrato BasicContractV1...");
+  // Desplegamos el contrato usando deployProxy con un inicializador
+  const proxy = await upgrades.deployProxy(
+    BasicContractV1,
+    [["Leer", "Escribir", "Borrar"]], // Pasamos un solo array que contiene las categorías,
+    {
+      initializer: "initialize", // Nombre de la función inicializadora
+    }
+  );
+  // Espera a que el despliegue sea confirmado
+  await proxy.deploymentTransaction()?.wait();
+  // Obtenemos la dirección del proxy
+  const proxyAddress = await proxy.getAddress();
+  console.log("Contrato proxy desplegado en:", proxyAddress);
+  // Verificar la categoria inicial
+  const categories = await proxy.getCategories();
+  console.log("Categoría inicial:", categories);
+  // Verificar el numero de tareas
+  const numTasks = await proxy.getTaskCount();
+  console.log("Número de tareas:", numTasks);
+  //Obtener las tareas
+  const tasks = await proxy.getTasks();
+  console.log("Tareas:", tasks);
+  /********* Deploy LogicV2 ********/
 }
 main().catch((error) => {
   console.error(error);
